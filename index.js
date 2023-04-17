fc1372ad2f5236012b4c72cda819db1bdff209eb
Chandrakant J Gawali <111560874+Chandrakant92@users.noreply.github.com>
2023-04-18 04:24:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,8 @@ const app = express();
 app.use(express.json())
 
 // Connect to MongoDB database
-// mongodb+srv://Deadpool:Deadpool@cluster0.skrk7kj.mongodb.net/?retryWrites=true&w=majority
 // mongodb+srv://agroinformatics:agroinformatics@agroinformatics.rmo37ed.mongodb.net/?retryWrites=true&w=majority
-mongoose.connect('mongodb+srv://Deadpool:Deadpool@cluster0.skrk7kj.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://agroinformatics:agroinformatics@agroinformatics.rmo37ed.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(error => console.error(error));
 
